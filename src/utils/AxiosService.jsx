092a468ca9897dbed1b65e0678a54e478b720074
ajithkumar2498 <ajibhc24@gmail.com>
2025-01-23 10:35:25 +0530

    backendurl updated

--- src/utils/AxiosService.jsx
+++ src/utils/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const AxiosService = axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://blogapp-backend-1-mezn.onrender.com",
     headers:{
         "Content-Type":"application/json"
     }
